cfb93f4f92a3e2ded915a40c81286a95287650aa
RafifWibowo <rafifnaufaldi2@gmail.com>
2024-12-25 11:23:22 +0700

    feat: add task 3 solution (show_total_salary_2021.sql)

---
+++ show_total_salary_2021.sql
@@ -0,0 +1,9 @@
+SELECT SUM(
+    CASE
+        WHEN release_date IS NULL THEN 12
+        WHEN release_date BETWEEN '2021-01-01' AND '2021-12-31' THEN 
+            MONTH(release_date) - MONTH(join_date) + 1
+        ELSE 0
+    END * salary
+) as total_salary_2021
+FROM employees;
