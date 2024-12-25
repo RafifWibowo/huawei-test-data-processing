99a31a29aee9997aad56f6fc55b3d858adacfc9f
RafifWibowo <rafifnaufaldi2@gmail.com>
2024-12-25 11:49:06 +0700

    feat: add shell script to run all files

---
+++ run_sql.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+read -p "Enter MySQL username: " username
+
+read -sp "Enter MySQL password: " password 
+
+
+# Define the order of SQL files in an array
+sql_files=("init_database.sql" "add_albert.sql" "update_engineer_salary.sql" "show_total_salary_2021.sql" "show_most_three_exp.sql" "show_engineer_less_three_exp.sql"
+)
+
+for file in "${sql_files[@]}"; do
+  if [[ -f "$file" ]]; then
+    echo "Executing: $file"
+    mysql -u "$username" -p"$password" < "$file"
+  else
+    echo "Warning: File '$file' not found."
+  fi
+done
